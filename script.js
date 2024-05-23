e5b2c39366158f5493b439e6a390ae47fbb81681
Anthony GODART <anthonygodart@MacBook-Pro-de-Anthony.local>
2024-05-23 13:53:01 +0200

    added reversed calc table

--- script.js
+++ script.js
@@ -17,8 +17,18 @@ document.getElementById('calc-btn').addEventListener('click', function() {
 
     // Calculs
     let shopPrice = tyreBuyPrice + tyreMargin;
-    let remise = ((shopPrice - tyreBF)/tyreBF)*100;
+    let remise = (( -1 * (shopPrice - tyreBF))/tyreBF)*100;
+    let remiseInf = Math.floor(remise);
+    let remiseSup = Math.ceil(remise);
+    // Calcul du prix remisé
+    let prixRemiseInf = (tyreBF - (tyreBF * (remiseInf / 100) )).toFixed(2);
+    let prixRemiseSup = (tyreBF - (tyreBF * (remiseSup / 100) )).toFixed(2);
+    // Calcul de la marge finale
+    let margeFinaleInf = ((tyreBF - (tyreBF * remiseInf) / 100) - tyreBuyPrice).toFixed(2);
+    let margeFinaleSup = ((tyreBF - (tyreBF * remiseSup) / 100) - tyreBuyPrice).toFixed(2);
 
+
+    // Gestion du tableau de calcul de départ
     // Création d'une nouvelle ligne dans le tableau
     let tbody = document.querySelector('#table tbody');
     let newRow = tbody.insertRow();
@@ -36,10 +46,45 @@ document.getElementById('calc-btn').addEventListener('click', function() {
     cells[2].textContent = shopPrice.toFixed(2) + "€";
     cells[3].textContent = tyreBF.toFixed(2) + "€";
     cells[4].innerHTML = "<span style='font-weight: bold; color: green;'>" + remise.toFixed(2) + "%</span>";
+
+    // Gestion du tableau de calcul en inversé
+    // Création des nouvelles lignes dans le tableau
+    let reversedtbody = document.querySelector('#reversed-table tbody');
+    let newRowInf = reversedtbody.insertRow();
+    let newRowSup = reversedtbody.insertRow();
+
+    // Ajoute les cellules à la ligne pour l'arrondi inférieur
+    let cellsInf = [
+        newRowInf.insertCell(),
+        newRowInf.insertCell(),
+        newRowInf.insertCell(),
+        newRowInf.insertCell()
+    ];
+
+    // Ajoute les cellules à la ligne pour l'arrondi supérieur
+    let cellsSup = [
+        newRowSup.insertCell(),
+        newRowSup.insertCell(),
+        newRowSup.insertCell(),
+        newRowSup.insertCell()
+    ];
+
+    // Remplissage des cellules avec les données calculées
+    cellsInf[0].rowSpan = 2;
+    cellsInf[0].textContent = tyreBF.toFixed(2) + "€";
+    cellsInf[1].textContent = remiseInf + "%";
+    cellsInf[2].textContent = prixRemiseInf + "€";
+    cellsInf[3].innerHTML = "<span style='font-weight: bold; color: green;'>" + margeFinaleInf + "€</span>";
+
+    cellsSup[0].textContent = remiseSup + "%";
+    cellsSup[1].textContent = prixRemiseSup + "€";
+    cellsSup[2].innerHTML = "<span style='font-weight: bold; color: red;'>" + margeFinaleSup + "€</span>";
 });
 
 document.getElementById('clear-btn').addEventListener('click', function(){
     // Effacement des lignes ajoutées
     let tbody = document.querySelector('#table tbody');
+    let reversedtbody = document.querySelector('#reversed-table tbody');
     tbody.innerHTML = '';
+    reversedtbody.innerHTML='';
 });
