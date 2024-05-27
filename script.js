04dec23ad2c00886befa6a1cd9b2e234339e535c
Anthony GODART <anthonygodart@MacBook-Pro-de-Anthony.local>
2024-05-27 13:22:29 +0200

    added brand input

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.getElementById('calc-btn').addEventListener('click', function() {
     // Récupération des valeurs des champs de saisie
+    let tyreBrandInput = document.getElementById("tyre-brand")
     let tyreBuyPriceInput = document.getElementById('tyre-buy-price');
     let tyreMarginInput = document.getElementById('tyre-margin');
     let tyreBFInput = document.getElementById('tyre-BF');
 
     // Vérification si les champs sont vides
-    if (tyreBuyPriceInput.value === '' || tyreMarginInput.value === '' || tyreBFInput.value === '') {
+    if (tyreBrandInput.value === '' || tyreBuyPriceInput.value === '' || tyreMarginInput.value === '' || tyreBFInput.value === '') {
         alert('Veuillez remplir tous les champs.');
         return; // Arrête l'exécution de la fonction si un champ est vide
     }
+
+    let tyreBrand = tyreBrandInput.value;
 
     // Conversion des valeurs en nombres flottants
     let tyreBuyPrice = parseFloat(tyreBuyPriceInput.value);
@@ -37,15 +40,17 @@ document.getElementById('calc-btn').addEventListener('click', function() {
         newRow.insertCell(),
         newRow.insertCell(),
         newRow.insertCell(),
+        newRow.insertCell(),
         newRow.insertCell()
     ];
 
     // Remplissage des cellules avec les données calculées
-    cells[0].textContent = tyreBuyPrice.toFixed(2) + "€";
-    cells[1].textContent = tyreMargin.toFixed(2) + "€";
-    cells[2].textContent = shopPrice.toFixed(2) + "€";
-    cells[3].textContent = tyreBF.toFixed(2) + "€";
-    cells[4].innerHTML = "<span style='font-weight: bold; color: green;'>" + remise.toFixed(2) + "%</span>";
+    cells[0].textContent = tyreBrand;
+    cells[1].textContent = tyreBuyPrice.toFixed(2) + "€";
+    cells[2].textContent = tyreMargin.toFixed(2) + "€";
+    cells[3].textContent = shopPrice.toFixed(2) + "€";
+    cells[4].textContent = tyreBF.toFixed(2) + "€";
+    cells[5].innerHTML = "<span style='font-weight: bold; color: green;'>" + remise.toFixed(2) + "%</span>";
 
     // Gestion du tableau de calcul en inversé
     // Création des nouvelles lignes dans le tableau
@@ -55,6 +60,7 @@ document.getElementById('calc-btn').addEventListener('click', function() {
 
     // Ajoute les cellules à la ligne pour l'arrondi inférieur
     let cellsInf = [
+        newRowInf.insertCell(),
         newRowInf.insertCell(),
         newRowInf.insertCell(),
         newRowInf.insertCell(),
@@ -71,14 +77,15 @@ document.getElementById('calc-btn').addEventListener('click', function() {
 
     // Remplissage des cellules avec les données calculées
     cellsInf[0].rowSpan = 2;
-    cellsInf[0].textContent = tyreBF.toFixed(2) + "€";
-    cellsInf[1].textContent = remiseInf + "%";
-    cellsInf[2].textContent = prixRemiseInf + "€";
-    cellsInf[3].innerHTML = "<span style='font-weight: bold; color: green;'>" + margeFinaleInf + "€</span>";
+    cellsInf[0].textContent = tyreBrand;
+    cellsInf[1].textContent = tyreBF.toFixed(2) + "€";
+    cellsInf[2].textContent = remiseInf + "%";
+    cellsInf[3].textContent = prixRemiseInf + "€";
+    cellsInf[4].innerHTML = "<span style='font-weight: bold; color: green;'>" + margeFinaleInf + "€</span>";
 
-    cellsSup[0].textContent = remiseSup + "%";
-    cellsSup[1].textContent = prixRemiseSup + "€";
-    cellsSup[2].innerHTML = "<span style='font-weight: bold; color: red;'>" + margeFinaleSup + "€</span>";
+    cellsSup[1].textContent = remiseSup + "%";
+    cellsSup[2].textContent = prixRemiseSup + "€";
+    cellsSup[3].innerHTML = "<span style='font-weight: bold; color: red;'>" + margeFinaleSup + "€</span>";
 });
 
 document.getElementById('clear-btn').addEventListener('click', function(){
